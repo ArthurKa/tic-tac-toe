a7b18bd2d99e968181ce41716356df705f7fa332
ArthurKa <arthur.katruk@gmail.com>
2023-06-12 12:50:39 +0300

    chore: fixate node version to v8.x.x in `start-cheks.js`

--- helpers/start-checks.js
+++ helpers/start-checks.js
@@ -7,14 +7,14 @@ const { execSync } = require('child_process');
 
 const nvmrc = fs.readFileSync(path.resolve('.nvmrc'), 'utf-8').trim();
 
-const nodeVersion = process.versions.node;
-if(nodeVersion !== nvmrc) {
-  throw new Error(`You must use Node.js v${nvmrc} for this project (${nodeVersion} used)`);
+const { node } = process.versions;
+if(node !== nvmrc) {
+  throw new Error(`You must use Node.js v${nvmrc} for this project (${node} used)`);
 }
 
 const npmVersion = execSync('npm -v').toString().trim();
-if(!(Number(npmVersion.split('.')[0]) >= 8)) {
-  throw new Error(`You must use NPM v8.x.x of higher (${npmVersion} used)`);
+if(Number(npmVersion.split('.')[0]) !== 8) {
+  throw new Error(`You must use NPM v8.x.x (${npmVersion} used)`);
 }
 
 const vscodeSettings = JSON.parse(fs.readFileSync(path.resolve('./.vscode/settings.json'), 'utf-8'));
